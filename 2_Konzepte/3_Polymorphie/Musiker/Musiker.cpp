a074c46018b614da4d076d7eca35b9cf9b3605f9
Matthias <Matthias-w5@outlook.de>
2023-05-21 21:12:07 +0200

    added musician has instrument check

--- 2_Konzepte/3_Polymorphie/Musiker/Musiker.cpp
+++ 2_Konzepte/3_Polymorphie/Musiker/Musiker.cpp
@@ -15,22 +15,30 @@ Musiker::~Musiker(void)
 {
     std::cout << "Destruktor: ~Musiker(): " << name << std::endl;
     delete instrument;
-
 }
 
 void Musiker::weiseInstrumentZu(Musikinstrument const *musikinstr)
 {
-    instrument = musikinstr;
-    std::cout << name << " erhaelt " << musikinstr->liefereName() << std::endl;
+    if (instrument == NULL)
+    {
+        instrument = musikinstr;
+        std::cout << name << " erhaelt " << musikinstr->liefereName() << std::endl;
+    }
+    else if (instrument != NULL)
+    {
+        delete instrument;
+        instrument = musikinstr;
+        std::cout << name << " erhaelt " << musikinstr->liefereName() << std::endl;
+    }
+    
 }
-
 void Musiker::spielen() const
 {
     if (instrument != NULL)
     {
         std::cout << name << " spielt " << instrument->liefereName() << ":" << std::endl;
         instrument->spielen();
-     }
+    }
 }
 
 std::string Musiker::liefereName() const
