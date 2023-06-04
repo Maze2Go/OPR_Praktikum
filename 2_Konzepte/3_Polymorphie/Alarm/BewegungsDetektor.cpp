a5118793b527af28e18b60b9738d315c476c89b0
Matthias <Matthias-w5@outlook.de>
2023-06-04 10:38:26 +0200

    moved destructor implementation to DetekorImpl

--- 2_Konzepte/3_Polymorphie/Alarm/BewegungsDetektor.cpp
+++ 2_Konzepte/3_Polymorphie/Alarm/BewegungsDetektor.cpp
@@ -6,12 +6,7 @@ BewegungsDetektor::BewegungsDetektor(std::string name) : DetektorImpl(name)
 }
 BewegungsDetektor::~BewegungsDetektor()
 {
-    std::cout <<"Detektor " << liefereName() << " abbauen" << std::endl;
-        
-    for(unsigned int i = 0; i < empfaenger.size(); i++)
-    {
-        delete empfaenger[i];
-    }
+ 
 }
 
 void BewegungsDetektor::ausloesen()
--- 2_Konzepte/3_Polymorphie/Alarm/DetektorImpl.cpp
+++ 2_Konzepte/3_Polymorphie/Alarm/DetektorImpl.cpp
@@ -5,15 +5,19 @@
 DetektorImpl::DetektorImpl(std::string name)
 {
     this->name = name;
-     std::cout << "Detektor " << liefereName() << " aufbauen" << std::endl;
+    std::cout << "Detektor " << liefereName() << " aufbauen" << std::endl;
+    for (unsigned int i = 0; i < empfaenger.size(); i++)
+    {
+        delete empfaenger[i];
+    }
 }
 
 DetektorImpl::~DetektorImpl()
 {
-    // ToDo
+    std::cout << "Detekror " << liefereName() << " abbauen" << std::endl;
 }
 
-void DetektorImpl::anschliessen(Alarmierbar& zu_alarmierendes_geraet)
+void DetektorImpl::anschliessen(Alarmierbar &zu_alarmierendes_geraet)
 {
     std::cout << zu_alarmierendes_geraet.liefereName() << " an " << this->liefereName() << " anschliessen" << std::endl;
     empfaenger.push_back(&zu_alarmierendes_geraet);
@@ -26,5 +30,5 @@ std::string DetektorImpl::liefereName() const
 
 void DetektorImpl::ausloesen()
 {
-    //ToDo
+    // ToDo
 }
