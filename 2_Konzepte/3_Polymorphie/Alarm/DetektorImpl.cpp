e5e9b61c1907e3f10057f098b0c4d5d61af2bc2b
Matthias <Matthias-w5@outlook.de>
2023-06-04 10:11:52 +0200

    added connection message to anschliessen()

--- 2_Konzepte/3_Polymorphie/Alarm/DetektorImpl.cpp
+++ 2_Konzepte/3_Polymorphie/Alarm/DetektorImpl.cpp
@@ -16,6 +16,7 @@ DetektorImpl::~DetektorImpl()
 
 void DetektorImpl::anschliessen(Alarmierbar& zu_alarmierendes_geraet)
 {
+    std::cout << zu_alarmierendes_geraet.liefereName() << " an " << this->liefereName() << " anschliessen" << std::endl;
     empfaenger.push_back(&zu_alarmierendes_geraet);
 }
 
