e1bbda464ab271ccd21c8a44b0bf12c16bec31a4
Matthias <Matthias-w5@outlook.de>
2023-06-10 19:33:17 +0200

    implenation comlpete except exerscice 10

--- 3_Vertiefung/Operatoren/Atomkern/Atomkern.cpp
+++ 3_Vertiefung/Operatoren/Atomkern/Atomkern.cpp
@@ -7,21 +7,21 @@ Atomkern::Atomkern(int Protonen, int Neutronen, std::string Elementsymbol)
     this->Elementsymbol = Elementsymbol;
 }
 
-int Atomkern::liefere_neutronen()
+int Atomkern::liefere_neutronen() const
 {
     return anzahl_neutronen;
 }
 
-int Atomkern::liefereOrdungszahl()
+int Atomkern::liefereOrdungszahl() const
 {
     return anzahl_protonen;
 }
-int Atomkern::liefereMassezahl()
+int Atomkern::liefereMassezahl() const
 {
     return (anzahl_neutronen + anzahl_protonen);
 }
 
-std::string Atomkern::liefereElementsymbol()
+std::string Atomkern::liefereElementsymbol() const
 {
     return Elementsymbol;
 }
@@ -40,9 +40,8 @@ Atomkern Atomkern::operator+=(const Atomkern &Fusions_Atomkern)
 {
     this->anzahl_protonen = anzahl_protonen + Fusions_Atomkern.anzahl_protonen;
     this->anzahl_neutronen = anzahl_neutronen + Fusions_Atomkern.anzahl_neutronen;
-     return(Atomkern{anzahl_protonen + Fusions_Atomkern.anzahl_protonen, anzahl_neutronen + anzahl_neutronen});
+    return (Atomkern{anzahl_protonen + Fusions_Atomkern.anzahl_protonen, anzahl_neutronen + anzahl_neutronen});
 }
-
 
 Atomkern Atomkern::operator-(const Atomkern &Fusions_Atomkern)
 {
@@ -53,5 +52,31 @@ Atomkern Atomkern::operator-=(const Atomkern &Fusions_Atomkern)
 {
     this->anzahl_protonen = anzahl_protonen - Fusions_Atomkern.anzahl_protonen;
     this->anzahl_neutronen = anzahl_neutronen - Fusions_Atomkern.anzahl_neutronen;
-     return(Atomkern{anzahl_protonen - Fusions_Atomkern.anzahl_protonen, anzahl_neutronen - anzahl_neutronen});
+    return (Atomkern{anzahl_protonen - Fusions_Atomkern.anzahl_protonen, anzahl_neutronen - anzahl_neutronen});
 }
+bool Atomkern::operator==(const Atomkern &Kern)
+{
+    if((this->liefereOrdungszahl() == Kern.liefereOrdungszahl()) && (this->liefereMassezahl() == Kern.liefereMassezahl()))
+        return true;
+    else
+    return false;
+}
+
+// Globale-Operatoren
+Atomkern operator*(int ganzzahliges_skalar, const Atomkern &kern)
+{
+    return Atomkern{ganzzahliges_skalar * kern.liefere_neutronen(), ganzzahliges_skalar * kern.liefereOrdungszahl(), "Unbenannt"};
+}
+
+Atomkern operator*(const Atomkern &kern, int ganzzahliges_skalar)
+{
+    return Atomkern{ganzzahliges_skalar * kern.liefereOrdungszahl(), ganzzahliges_skalar * kern.liefere_neutronen(), "Unbenannt"};
+}
+
+std::ostream& operator<<(std::ostream& os, const Atomkern& Kern)
+{
+    os  << "Element: " << Kern.liefereElementsymbol() << ", Protonen: " << Kern.liefereOrdungszahl() << ", Neutronen: "
+        << Kern.liefere_neutronen() << ", Massenzahl: " << Kern.liefereMassezahl() << std::endl;
+    return os;
+}
+
--- 3_Vertiefung/Operatoren/Atomkern/Atomkern.h
+++ 3_Vertiefung/Operatoren/Atomkern/Atomkern.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 
 class Atomkern
 {
@@ -12,10 +13,10 @@ private:
 public:
     Atomkern(int Protonen, int Neutronen, std::string Elementsymbol = "");
     //= liefereMassezahl() int liefere_protonen();
-    int liefere_neutronen();
-    int liefereOrdungszahl();
-    int liefereMassezahl();
-    std::string liefereElementsymbol();
+    int liefere_neutronen() const;
+    int liefereOrdungszahl() const;
+    int liefereMassezahl() const;
+    std::string liefereElementsymbol() const;
 
     Atomkern kernfusion(const Atomkern &Fusions_AtomKern);
     Atomkern kernspaltung();
@@ -25,4 +26,10 @@ public:
     Atomkern operator+=(const Atomkern &Fusions_AtomKern);
     Atomkern operator-(const Atomkern &Fusions_AtomKern);
     Atomkern operator-=(const Atomkern &Fusions_AtomKern);
+    bool operator==(const Atomkern &Kern);
 };
+
+// Funktions-Prototypen
+Atomkern operator*(int ganzzahliges_skalar, const Atomkern &kern);
+Atomkern operator*(const Atomkern &kern, int ganzzahliges_skalar);
+std::ostream &operator<<(std::ostream &os, const Atomkern &Kern);
