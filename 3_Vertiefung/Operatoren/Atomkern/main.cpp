ed337ec348c77f854412f70253c49de3bfd9acab
Matthias <Matthias-w5@outlook.de>
2023-06-10 18:44:03 +0200

    added mahr templates

--- 3_Vertiefung/Operatoren/Atomkern/main.cpp
+++ 3_Vertiefung/Operatoren/Atomkern/main.cpp
@@ -1,20 +1,13 @@
-#include "Atomkern.h"
+/**
+ * OPR-Praktikum SS 2012
+ * Aufgabe 9
+ * @author Thomas Mahr
+ */
 
-#include <iostream>
+#include "PrueffolgeAtomkern.h"
 
-using namespace std;
-
-int main()
+int main() 
 {
-    Atomkern H(1, 0, "Wasserstoff");
-    Atomkern O(8, 8, "Sauerstoff");
-    Atomkern H20(0, 0, "Wasser");
-
-    H20 += H;
-
-    cout << "Name: " << H20.liefereElementsymbol() << ", Ordungszahl: " << H20.liefereOrdungszahl() << ", Massezahl: " << H20.liefereMassezahl() << endl;
-
-    H20 += O+H;
-
-    cout << "Name: " << H20.liefereElementsymbol() << ", Ordungszahl: " << H20.liefereOrdungszahl() << ", Massezahl: " << H20.liefereMassezahl() << endl;
+	PrueffolgeAtomkern prueffolgeAtomkern;
+	prueffolgeAtomkern.ausfuehren();
 }
