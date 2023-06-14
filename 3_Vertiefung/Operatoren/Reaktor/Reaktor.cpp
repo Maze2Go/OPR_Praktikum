b9710add037d5b8ee92f0fa51e917f618b92afd2
Matthias <Matthias-w5@outlook.de>
2023-06-14 20:29:13 +0200

    working commit

--- 3_Vertiefung/Operatoren/Reaktor/Reaktor.cpp
+++ 3_Vertiefung/Operatoren/Reaktor/Reaktor.cpp
@@ -1,3 +1,37 @@
 #include "Reaktor.h"
 
+const Atomkern &Reaktor::reaktor_inhalt() const
+{
 
+    return inhalt_reaktor;
+}
+
+void Reaktor::operator~(void)
+{
+    inhalt_reaktor.Atomkernzuruecksetzen();
+}
+
+Reaktor &Reaktor::operator<(const Atomkern &kern)
+{
+
+    inhalt_reaktor += kern;
+    return *this;
+}
+
+Reaktor & Reaktor::operator>(const Atomkern &kern)
+{
+    inhalt_reaktor -= kern;
+    return *this;
+}
+
+const Atomkern &Reaktor::operator*(void)
+{
+    return this->reaktor_inhalt();
+}
+
+
+
+const Atomkern &operator*(const Reaktor &kern)
+{
+    return kern.reaktor_inhalt();
+}
--- 3_Vertiefung/Operatoren/Reaktor/Reaktor.h
+++ 3_Vertiefung/Operatoren/Reaktor/Reaktor.h
@@ -1,6 +1,21 @@
 #pragma once
 
+#include "Atomkern.h"
+
+#include <vector>
+
 class Reaktor
 {
+private:
+    Atomkern inhalt_reaktor;
 
+public:
+    const Atomkern &reaktor_inhalt() const;
+
+    void operator~();
+    Reaktor &operator<(const Atomkern &kern);
+    Reaktor &operator>(const Atomkern &kern);
+    const Atomkern &operator*(void);
 };
+
+const Atomkern &operator*(const Reaktor &kern);
